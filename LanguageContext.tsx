68ce6f524e49ef5bc9709d848a8229d62e5091c4
Peachsmash <69175810+Peachsmash@users.noreply.github.com>
2025-12-20 08:37:37 +0100

    Refactor imports in LanguageContext.tsx

--- LanguageContext.tsx
+++ LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React from 'react';
 import { Language } from './types';
 import { translations } from './translations';
 
