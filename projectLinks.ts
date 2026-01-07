8e056dc98c4611c0dc30e98e5c52539e34c5658b
Peachsmash <69175810+Peachsmash@users.noreply.github.com>
2026-01-07 14:55:54 +0100

    Add files via upload

---
+++ projectLinks.ts
@@ -0,0 +1,10 @@
+import { ProjectLinks } from '../types';
+
+export const projectLinks: Record<number, ProjectLinks> = {
+    1: { demo: "https://example.com" },
+    2: { demo: "#" },
+    3: { demo: "#" },
+    4: { demo: "https://example.com" },
+    5: { },
+    6: { demo: "#" },
+};
